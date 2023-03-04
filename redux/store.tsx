447ca92af135b8451313a0ee9e7fabd977e65b25
Edwin Giovanni <eperez@cxclatam.com>
2023-03-04 15:36:19 -0600

    delete reducer

--- redux/store.tsx
+++ redux/store.tsx
@@ -38,22 +38,3 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
-
-// const makeStore = () =>
-//   configureStore({
-//     reducer: {
-//       [authSlice.name]: authSlice.reducer,
-//     },
-//     devTools: true,
-//   });
-
-// export type AppStore = ReturnType<typeof makeStore>;
-// export type AppState = ReturnType<AppStore["getState"]>;
-// export type AppThunk<ReturnType = void> = ThunkAction<
-//   ReturnType,
-//   AppState,
-//   unknown,
-//   Action
-// >;
-
-// export const wrapper = createWrapper<AppStore>(makeStore);
